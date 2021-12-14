5ebf8350ecf572f26e8cb3c14db77387d6b11ae3
IClimber <0969966@gmail.com>
2021-12-14 03:09:41 +0200

    Fix header reading from file and update AssemblyInfo

--- CryptoDB/Properties/AssemblyInfo.cs
+++ CryptoDB/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Windows;
 // Управление общими сведениями о сборке осуществляется с помощью 
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("CryptoDataBase_3.0")]
+[assembly: AssemblyTitle("CryptoDataBase V4.0")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CryptoDataBase_3.0")]
+[assembly: AssemblyProduct("CryptoDataBase V4.0")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -49,5 +49,5 @@ using System.Windows;
 // Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
