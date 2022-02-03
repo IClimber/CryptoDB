45c67faff26afec045a8e069270875a80e2b70b6
IClimber <0969966@gmail.com>
2022-02-04 00:16:35 +0200

    Remember last isStretch for ImageViewer Window

--- CryptoDB/ImageViewer.xaml.cs
+++ CryptoDB/ImageViewer.xaml.cs
@@ -27,13 +27,17 @@ namespace CryptoDataBase
 		private double ZoomStep = 1.1;
 		private Transform originalTransform;
 		private readonly int Dpi = STANDART_DPI;
-		public bool IsStretch { get; set; } = false;
+		public bool IsStretch { get { return _isStretch; } set { _isStretch = value; _isStretchGlobal = value; } }
+		private bool _isStretch = false;
+		private static bool _isStretchGlobal = false;
 
 		private System.Windows.Controls.ListView parentListView;
 		BitmapImage bmp = null;
 
 		public ImageViewer()
 		{
+			IsStretch = _isStretchGlobal;
+
 			InitializeComponent();
 			Dpi = (int)(STANDART_DPI * (Screen.PrimaryScreen.Bounds.Width / SystemParameters.PrimaryScreenWidth));
 
