1b71f0dadd3472be4ef6708aa64e9e60c711376f
Yurii <0969966@gmail.com>
2025-08-16 12:42:24 +0300

    feat: resize ImageViewer fullscreen

--- CryptoDB/ImageViewer.xaml.cs
+++ CryptoDB/ImageViewer.xaml.cs
@@ -3,6 +3,7 @@ using ImageConverter;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Forms;
@@ -11,254 +12,288 @@ using System.Windows.Media.Imaging;
 
 namespace CryptoDataBase
 {
-	/// <summary>
-	/// Логика взаимодействия для DocSource.xaml
-	/// </summary>
-	public partial class ImageViewer : Window
-	{
-		int currentIndex;
-		List<Element> elements;
-		public bool IsStretch { get { return _isStretch; } set { _isStretch = value; _isStretchGlobal = value; } }
-		private bool _isStretch = false;
-		private static bool _isStretchGlobal = false;
-		private System.Windows.Controls.ListView parentListView;
+    /// <summary>
+    /// Логика взаимодействия для DocSource.xaml
+    /// </summary>
+    public partial class ImageViewer : Window
+    {
+        int currentIndex;
+        List<Element> elements;
+        public bool IsStretch { get { return _isStretch; } set { _isStretch = value; _isStretchGlobal = value; } }
+        private bool _isStretch = false;
+        private static bool _isStretchGlobal = false;
+        private System.Windows.Controls.ListView parentListView;
+        private bool isFullScreen = false;
+        private static readonly IntPtr HWND_TOP = new IntPtr(0);
+        private const uint SWP_SHOWWINDOW = 0x0040;
 
-		public ImageViewer()
-		{
-			IsStretch = _isStretchGlobal;
+        [DllImport("user32.dll")]
+        private static extern bool SetWindowPos(
+        IntPtr hWnd, IntPtr hWndInsertAfter,
+        int X, int Y, int cx, int cy, uint uFlags);
 
-			InitializeComponent();
+        public ImageViewer()
+        {
+            IsStretch = _isStretchGlobal;
 
-			toolPanel.Visibility = Visibility.Hidden;
-			prevImg.Visibility = Visibility.Hidden;
-			nextImg.Visibility = Visibility.Hidden;
-		}
+            InitializeComponent();
 
-		public ImageViewer(List<Element> elementList, FileElement current, System.Windows.Controls.ListView ParentListView) : this()
-		{
-			parentListView = ParentListView;
-			elements = elementList;
-			currentIndex = elementList.IndexOf(current);
-		}
+            toolPanel.Visibility = Visibility.Hidden;
+            prevImg.Visibility = Visibility.Hidden;
+            nextImg.Visibility = Visibility.Hidden;
+        }
 
-		private void Window_PreviewKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
-		{
-			if ((e.Key == Key.Escape) || (e.Key == Key.Back))
-			{
-				Close();
-			}
+        public ImageViewer(List<Element> elementList, FileElement current, System.Windows.Controls.ListView ParentListView) : this()
+        {
+            parentListView = ParentListView;
+            elements = elementList;
+            currentIndex = elementList.IndexOf(current);
+        }
 
-			if (e.Key == Key.Left)
-			{
-				ShowPrevImage();
-			}
+        private void Window_PreviewKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if ((e.Key == Key.Escape) || (e.Key == Key.Back))
+            {
+                Close();
+            }
 
-			if (e.Key == Key.Right)
-			{
-				ShowNextImage();
-			}
+            if (e.Key == Key.Left)
+            {
+                ShowPrevImage();
+            }
 
-			if ((Keyboard.Modifiers == ModifierKeys.Control) && (e.Key == Key.S))
-			{
-				SaveAs_Click(null, null);
-			}
+            if (e.Key == Key.Right)
+            {
+                ShowNextImage();
+            }
 
-			if (e.Key == Key.F2)
-			{
-				Rename_Click(null, null);
-			}
+            if ((Keyboard.Modifiers == ModifierKeys.Control) && (e.Key == Key.S))
+            {
+                SaveAs_Click(null, null);
+            }
 
-			if (e.Key == Key.Delete)
-			{
-				Delete_Click(null, null);
-			}
-		}
+            if (e.Key == Key.F2)
+            {
+                Rename_Click(null, null);
+            }
 
-		private void ShowNextImage()
-		{
-			currentIndex++;
-			if (currentIndex >= elements.Count)
-			{
-				currentIndex = 0;
-			}
-			ShowImage();
-		}
+            if (e.Key == Key.Delete)
+            {
+                Delete_Click(null, null);
+            }
+        }
 
-		private void ShowPrevImage()
-		{
-			currentIndex--;
-			if (currentIndex < 0)
-			{
-				currentIndex = elements.Count - 1;
-			}
-			ShowImage();
-		}
+        private void ShowNextImage()
+        {
+            currentIndex++;
+            if (currentIndex >= elements.Count)
+            {
+                currentIndex = 0;
+            }
+            ShowImage();
+        }
 
-		private string FormatingSize(UInt64 Size)
-		{
-			string[] sizes = { "B", "KB", "MB", "GB", "TB" };
-			double len = Size;
-			int order = 0;
-			while (len >= 1024 && order < sizes.Length - 1)
-			{
-				order++;
-				len = len / 1024;
-			}
+        private void ShowPrevImage()
+        {
+            currentIndex--;
+            if (currentIndex < 0)
+            {
+                currentIndex = elements.Count - 1;
+            }
+            ShowImage();
+        }
 
-			return String.Format("{0:0.##} {1}", len, sizes[order]);
-		}
+        private string FormatingSize(UInt64 Size)
+        {
+            string[] sizes = { "B", "KB", "MB", "GB", "TB" };
+            double len = Size;
+            int order = 0;
+            while (len >= 1024 && order < sizes.Length - 1)
+            {
+                order++;
+                len = len / 1024;
+            }
 
-		private void ShowImage()
-		{
-			if ((currentIndex < 0) || (currentIndex >= elements.Count))
-			{
-				return;
-			}
+            return String.Format("{0:0.##} {1}", len, sizes[order]);
+        }
 
-			FileElement currentElement = elements[currentIndex] as FileElement;
-			parentListView.SelectedItem = currentElement;
-			parentListView.ScrollIntoView(parentListView.SelectedItem);
-			Title = currentElement.Name;
+        private void ShowImage()
+        {
+            if ((currentIndex < 0) || (currentIndex >= elements.Count))
+            {
+                return;
+            }
 
-			MemoryStream ms = new MemoryStream();
-			BitmapImage bmp = null;
+            FileElement currentElement = elements[currentIndex] as FileElement;
+            parentListView.SelectedItem = currentElement;
+            parentListView.ScrollIntoView(parentListView.SelectedItem);
+            Title = currentElement.Name;
 
-			try
-			{
-				currentElement.SaveTo(ms);
-				ms.Position = 0;
-				bmp = ImgConverter.StreamToBitmapImage(ms);
-				image.SetSource(bmp);
-				image.IsStretch = IsStretch;
+            MemoryStream ms = new MemoryStream();
+            BitmapImage bmp = null;
+
+            try
+            {
+                currentElement.SaveTo(ms);
+                ms.Position = 0;
+                bmp = ImgConverter.StreamToBitmapImage(ms);
+                image.SetSource(bmp);
+                image.IsStretch = IsStretch;
                 //bmp = BitmapFrame.Create(ms, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.OnLoad);
                 //ImageBehavior.SetAnimatedSource(image, bmp);
                 //bmp = null;
             }
-			catch
-			{
+            catch
+            {
 
-			}
+            }
 
             ms.Dispose();
 
             TextBlockStatus1.Text = "Image size: " + FormatingSize(currentElement.Size);
-			TextBlockStatus2.Text = "Image resolution: " + bmp?.PixelWidth + " x " + bmp?.PixelHeight;
-			TextBlockStatus3.Text = "DPI: X=" + (int?)bmp?.DpiX + "  Y=" + (int?)bmp?.DpiY;
-			TextBlockStatus4.Text = (currentIndex + 1).ToString() + @" / " + elements.Count.ToString();
-		}
+            TextBlockStatus2.Text = "Image resolution: " + bmp?.PixelWidth + " x " + bmp?.PixelHeight;
+            TextBlockStatus3.Text = "DPI: X=" + (int?)bmp?.DpiX + "  Y=" + (int?)bmp?.DpiY;
+            TextBlockStatus4.Text = (currentIndex + 1).ToString() + @" / " + elements.Count.ToString();
+        }
 
-		private void Rotate_Click(object sender, RoutedEventArgs e)
-		{
-			double angle = int.Parse((string)((System.Windows.Controls.Button)sender).Tag);
-			if (angle == 90)
-			{
-				image.Rotate90();
-			}
+        private void Rotate_Click(object sender, RoutedEventArgs e)
+        {
+            double angle = int.Parse((string)((System.Windows.Controls.Button)sender).Tag);
+            if (angle == 90)
+            {
+                image.Rotate90();
+            }
 
-			if (angle == 270)
-			{
-				image.Rotate270();
-			}
-		}
+            if (angle == 270)
+            {
+                image.Rotate270();
+            }
+        }
 
-		private void grid_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
-		{
-			toolPanel.Visibility = (e.GetPosition(sender as IInputElement).Y < 50) ? Visibility.Visible : Visibility.Hidden;
+        private void grid_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            toolPanel.Visibility = (e.GetPosition(sender as IInputElement).Y < 50) ? Visibility.Visible : Visibility.Hidden;
 
-			prevImg.Visibility = (e.GetPosition(sender as IInputElement).X <= prevImgColumn.Width.Value) ? Visibility.Visible : Visibility.Hidden;
-			nextImg.Visibility = ((grid.RenderSize.Width - e.GetPosition(sender as IInputElement).X) <= prevImgColumn.Width.Value) ? Visibility.Visible : Visibility.Hidden;
-		}
+            prevImg.Visibility = (e.GetPosition(sender as IInputElement).X <= prevImgColumn.Width.Value) ? Visibility.Visible : Visibility.Hidden;
+            nextImg.Visibility = ((grid.RenderSize.Width - e.GetPosition(sender as IInputElement).X) <= prevImgColumn.Width.Value) ? Visibility.Visible : Visibility.Hidden;
+        }
 
-		private void Image_MouseDown(object sender, MouseButtonEventArgs e)
-		{
-			byte direct = (sender is Border) ? Convert.ToByte((sender as Border).Tag) : Convert.ToByte((sender as Image).Tag);
+        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            byte direct = (sender is Border) ? Convert.ToByte((sender as Border).Tag) : Convert.ToByte((sender as Image).Tag);
 
-			switch (direct)
-			{
-				case 0: ShowPrevImage(); break;
-				case 1: ShowNextImage(); break;
-			}
-		}
+            switch (direct)
+            {
+                case 0: ShowPrevImage(); break;
+                case 1: ShowNextImage(); break;
+            }
+        }
 
-		private void SaveAs_Click(object sender, RoutedEventArgs e)
-		{
-			SaveFileDialog sd = new SaveFileDialog();
-			sd.FileName = elements[currentIndex].Name;
-			sd.DefaultExt = Path.GetExtension(elements[currentIndex].Name);
-			sd.Filter = "Image (*." + sd.DefaultExt.ToUpper() + ")|*." + sd.DefaultExt.ToUpper();
-			if (sd.ShowDialog() != System.Windows.Forms.DialogResult.OK)
-			{
-				return;
-			}
+        private void SaveAs_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog sd = new SaveFileDialog();
+            sd.FileName = elements[currentIndex].Name;
+            sd.DefaultExt = Path.GetExtension(elements[currentIndex].Name);
+            sd.Filter = "Image (*." + sd.DefaultExt.ToUpper() + ")|*." + sd.DefaultExt.ToUpper();
+            if (sd.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+            {
+                return;
+            }
 
-			elements[currentIndex].SaveAs(sd.FileName);
-		}
+            elements[currentIndex].SaveAs(sd.FileName);
+        }
 
-		private void Rename_Click(object sender, RoutedEventArgs e) //провіряти чи MainWindow editable == true
-		{
-			if ((Owner as MainWindow).IsReadOnly)
-			{
-				return;
-			}
+        private void Rename_Click(object sender, RoutedEventArgs e) //провіряти чи MainWindow editable == true
+        {
+            if ((Owner as MainWindow).IsReadOnly)
+            {
+                return;
+            }
 
-			RenameWindow renamer = new RenameWindow(elements[currentIndex].Name, elements[currentIndex].Type) { Owner = this };
-			if (renamer.ShowDialog() == true)
-			{
-				try
-				{
-					elements[currentIndex].Name = renamer.textBox.Text;
-				}
-				catch (Exception ex)
-				{
-					System.Windows.MessageBox.Show(ex.Message);
-					return;
-				}
+            RenameWindow renamer = new RenameWindow(elements[currentIndex].Name, elements[currentIndex].Type) { Owner = this };
+            if (renamer.ShowDialog() == true)
+            {
+                try
+                {
+                    elements[currentIndex].Name = renamer.textBox.Text;
+                }
+                catch (Exception ex)
+                {
+                    System.Windows.MessageBox.Show(ex.Message);
+                    return;
+                }
 
-				Title = elements[currentIndex].Name;
-			}
-		}
+                Title = elements[currentIndex].Name;
+            }
+        }
 
-		private void Delete_Click(object sender, RoutedEventArgs e)
-		{
-			if ((Owner as MainWindow).IsReadOnly)
-			{
-				return;
-			}
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            if ((Owner as MainWindow).IsReadOnly)
+            {
+                return;
+            }
 
-			if (System.Windows.MessageBox.Show(this, "Удалить елемент", "Удаление", MessageBoxButton.OKCancel) != MessageBoxResult.OK)
-			{
-				return;
-			}
+            if (System.Windows.MessageBox.Show(this, "Удалить елемент", "Удаление", MessageBoxButton.OKCancel) != MessageBoxResult.OK)
+            {
+                return;
+            }
 
-			if (Owner is MainWindow)
-			{
-				if ((Owner as MainWindow).DeleteElement(elements[currentIndex]))
-				{
-					elements.RemoveAt(currentIndex);
-					currentIndex--;
-					ShowNextImage();
-				}
+            if (Owner is MainWindow)
+            {
+                if ((Owner as MainWindow).DeleteElement(elements[currentIndex]))
+                {
+                    elements.RemoveAt(currentIndex);
+                    currentIndex--;
+                    ShowNextImage();
+                }
 
-				if ((currentIndex < 0) || (currentIndex >= elements.Count))
-				{
-					Close();
-				}
-			}
-		}
+                if ((currentIndex < 0) || (currentIndex >= elements.Count))
+                {
+                    Close();
+                }
+            }
+        }
 
-		private void Window_Closed(object sender, EventArgs e)
-		{
-			GC.Collect();
-		}
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            GC.Collect();
+        }
 
-		private void IsStretchCheckBox_Click(object sender, RoutedEventArgs e)
-		{
-			image.IsStretch = IsStretch;
-		}
+        private void IsStretchCheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            image.IsStretch = IsStretch;
+        }
 
-		private void imageViewer_Loaded(object sender, RoutedEventArgs e)
-		{
-			ShowImage();
-		}
-	}
+        private void imageViewer_Loaded(object sender, RoutedEventArgs e)
+        {
+            ShowImage();
+        }
+
+        private void image_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            isFullScreen = !isFullScreen;
+
+            if (isFullScreen)
+            {
+                WindowStyle = WindowStyle.None;
+                ResizeMode = ResizeMode.NoResize;
+                WindowState = WindowState.Maximized;
+
+                var screen = Screen.PrimaryScreen.Bounds;
+                var handle = new System.Windows.Interop.WindowInteropHelper(this).Handle;
+                SetWindowPos(handle, HWND_TOP,
+                screen.Left, screen.Top,
+                screen.Width, screen.Height,
+                SWP_SHOWWINDOW);
+            }
+            else
+            {
+                WindowStyle = WindowStyle.SingleBorderWindow;
+                ResizeMode = ResizeMode.CanResizeWithGrip;
+            }
+
+            Topmost = isFullScreen;
+        }
+    }
 }
